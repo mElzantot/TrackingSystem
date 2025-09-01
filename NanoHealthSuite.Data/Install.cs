c526c63319540ba98dc86f71b276b985e17438ae
Mohamed Shaker <mohamed.shaker@originsysglobal.com>
2025-09-02 01:44:59 +0300

    install db file has been added

---
+++ NanoHealthSuite.Data/Install.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WorkflowTracking.Infrastructure.Data;
+
+namespace NanoHealthSuite.Data;
+
+public static class Install
+{
+    public static IServiceCollection AddWorkflowContextServices(this IServiceCollection services, IConfiguration configuration, ILogger? logger = null)
+    {
+        var connString = configuration.GetConnectionString("WorkflowDb");
+
+        logger?.LogInformation("*** Using Connection string: " + connString);
+        services.AddDbContext<WorkflowDbContext>(options =>
+        {
+            options
+                .UseNpgsql(connString, config => config.EnableRetryOnFailure(3))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+                .ConfigureWarnings(warn =>
+                    warn.Throw(RelationalEventId.QueryPossibleUnintendedUseOfEqualsWarning)
+                );
+        }, ServiceLifetime.Transient);
+
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        return services;
+    }
+}
