e76e46cf0301dbbac8d739f15bc2200087e1d620
Mohamed Shaker <mohamed.shaker@originsysglobal.com>
2025-09-02 10:56:45 +0300

    program file has been updated

--- NanoHealthSuite.TrackingSystem.Api/Program.cs
+++ NanoHealthSuite.TrackingSystem.Api/Program.cs
@@ -1,12 +1,47 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 using NanoHealthSuite.TrackingSystem;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON serialization
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Workflow Tracking API",
+        Version = "v1",
+        Description = "API for managing workflows and process tracking"
+    });
+    c.UseInlineDefinitionsForEnums();
+});
+    
 builder.Services.AddWorkflowServices(builder.Configuration);
+
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 
 var app = builder.Build();
 
@@ -14,8 +49,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "workflow/swagger/{documentname}/swagger.json";
+    });
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/workflow/swagger/v1/swagger.json", "workflow docs V1");
+        options.RoutePrefix = "workflow/swagger";
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
