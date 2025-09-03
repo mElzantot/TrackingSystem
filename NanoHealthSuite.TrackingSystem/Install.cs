e959f14db729f589351306ae0bf3fb42539fba49
Mohamed Shaker <mohamed.shaker@originsysglobal.com>
2025-09-03 10:06:33 +0300

    install file has been updated

--- NanoHealthSuite.TrackingSystem/Install.cs
+++ NanoHealthSuite.TrackingSystem/Install.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using NanoHealthSuite.Data;
+using NanoHealthSuite.Data.Enums;
 using NanoHealthSuite.TrackingSystem.Helpers;
+using NanoHealthSuite.TrackingSystem.Managers;
 using NanoHealthSuite.TrackingSystem.Processors;
 using NanoHealthSuite.TrackingSystem.Services;
+using NanoHealthSuite.Validation;
 
 namespace NanoHealthSuite.TrackingSystem;
 
@@ -15,13 +21,31 @@ public static class Install
         IConfiguration configuration, 
         ILogger? logger = null)
     {
+        services.AddHttpClient();
         services.AddScoped<IHashingService, HashingService>();
         services.AddScoped<ITokenServiceProvider, TokenServiceProvider>();
         services.AddScoped<AuthenticationService>();
+        
+        // Register validation strategies
+        services.AddScoped<ApiValidationStrategy>();
+
+        var validationTypes = new Dictionary<CustomValidationType, Type>
+            { { CustomValidationType.API, typeof(ApiValidationStrategy) } };
+
+        services
+            .AddScoped<Func<CustomValidationType, IProcessStepValidationStrategy>>(serviceProvider =>
+                validationValue => GetSafeValidationStrategytHandler(serviceProvider, validationTypes, validationValue));
+        
+        services.AddScoped<ValidationService>();
         services.AddScoped<WorkflowService>();
+        services.AddScoped<ProcessService>();
+        services.AddScoped<ProcessHelper>();
+        services.AddScoped<ProcessManager>();
+
         services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
 
         services.AddWorkflowContextServices(configuration, logger);
+        
         return services;
     }
 
@@ -61,6 +85,18 @@ public static class Install
                     }
                 };
             });
+    }
+    
+    static IProcessStepValidationStrategy? GetSafeValidationStrategytHandler(
+        IServiceProvider serviceProvider,
+        Dictionary<CustomValidationType, Type> validationStrategies,
+        CustomValidationType validationType)
+    {
+        var validationValue = validationStrategies?.GetValueOrDefault(validationType);
+
+        return validationValue != null
+            ? serviceProvider.GetRequiredService(validationValue) as IProcessStepValidationStrategy
+            : null;
     }
 
 }
