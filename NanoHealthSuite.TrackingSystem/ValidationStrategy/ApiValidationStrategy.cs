98befc3d43cc47fa4e05da1171ebb2a4d879b9bc
Mohamed Shaker <mohamed.shaker@originsysglobal.com>
2025-09-04 13:02:26 +0300

    bug/ handle exception when calling external apis

--- NanoHealthSuite.TrackingSystem/ValidationStrategy/ApiValidationStrategy.cs
+++ NanoHealthSuite.TrackingSystem/ValidationStrategy/ApiValidationStrategy.cs
@@ -13,38 +13,44 @@ public class ApiValidationStrategy : HttpApiClient, IProcessStepValidationStrate
     public ApiValidationStrategy(HttpClient _httpClient) :base(_httpClient)
     {
     }
-    
+
     public async Task<Result<string>> ValidateAsync(
         Dictionary<string, string> data,
         CustomValidation validation)
     {
-        var apiData = JsonSerializer.Deserialize<ApiValidationData>(JsonSerializer.Serialize(data));
-        
-        if(string.IsNullOrEmpty(apiData?.Url))
+        var apiData = JsonSerializer.Deserialize<ApiValidationData>(validation.Data);
+
+        if (string.IsNullOrEmpty(apiData?.Url))
         {
             return Result.Fail<string>("Api validation requires a URL");
         }
 
         var url = ReplacePlaceholders(apiData.Url, data);
         var payload = ReplacePlaceholders(apiData.PayloadTemplate, data);
-        
-        // Assumed This Response Body for Simplicity
-        var response = await PostAsync(
-            endpoint: url,
-            response: new
-            {
-                status = string.Empty,
-                Error = string.Empty
-            },
-            headers: apiData.Headers,
-            payload: JsonSerializer.Deserialize<object>(payload));
 
-        return response.status == "200"
-            ? Result.Ok(JsonSerializer.Serialize(response))
-            : Result.Fail<string>(response.Error);
+        try
+        {
+            // Assumed This Response Body for Simplicity --> We can generalize it later
+            var response = await PostAsync(
+                endpoint: url,
+                response: new
+                {
+                    status = string.Empty,
+                    Error = string.Empty
+                },
+                headers: apiData.Headers,
+                payload: payload == null ? null : JsonSerializer.Deserialize<object>(payload));
+
+            return response.status == "200"
+                ? Result.Ok(JsonSerializer.Serialize(response))
+                : Result.Fail<string>(response.Error);
+        }
+        catch (Exception e)
+        {
+            return Result.Fail<string>($"Error While Calling External url: {url} : {e.Message}");
+        }
     }
-    
-    
+
     //Here I handled a simple payload template where variables will be like this "{{processId}}"
     private static string ReplacePlaceholders(string template, Dictionary<string, string> values)
     {
